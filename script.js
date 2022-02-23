537fa47a72381ddd1df59f811e768fd9b2a73946
Lauren Castro <laurcastro.l.c@gmail.com>
2022-02-23 15:45:41 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,31 @@
+alert("The name Lauren starts with the letter L")
+function firstLetterName(name)
+            {
+                alert("The name " + name + " starts with the letter " + name[0]);
+               
+            }
+            
+function divisibleByTwo(value)
+            {
+                if(value % 2 === 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+function largestNum(arr)
+            {
+                max = arr[0];
+
+                for(i = 0; i < arr.length; i++)
+                {
+                    if(max < arr[i])
+                    {
+                        max = arr[i];
+                    }
+                }
+
+                return max; 
+            }}
